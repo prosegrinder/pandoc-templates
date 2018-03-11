9f0bb98711bed6b59ec5df8dd5791d76efbf307a
David L. Day <dday376@gmail.com>
2018-03-11 00:55:09 -0500

    Do work in a temporary directory to make things safer.

--- bin/shunnshortstory.sh
+++ bin/shunnshortstory.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# Figure out where everything is
 SCRIPT=$(realpath $0)
 SCRIPT_PATH=$(dirname $SCRIPT)
 PANDOC_TEMPLATES=$(dirname $SCRIPT_PATH)
-export PANDOC_DATA_DIR="$PANDOC_TEMPLATES/shunn/shortstory"
-pandoc $1 --from markdown --to docx --lua-filter $PANDOC_DATA_DIR/shortstory.lua --data-dir $PANDOC_DATA_DIR --output $2
+SHUNN_SHORT_STORY_DIR="$PANDOC_TEMPLATES/shunn/shortstory"
+
+# Create a temporary data directory
+export PANDOC_DATA_DIR=$(mktemp -d)
+# Copy the template.docx file to the temporary directory
+cp $SHUNN_SHORT_STORY_DIR/template.docx $PANDOC_DATA_DIR/template.docx
+
+# Run pandoc
+pandoc $1 --from markdown --to docx --lua-filter $SHUNN_SHORT_STORY_DIR/shortstory.lua --data-dir $PANDOC_DATA_DIR --output $2
+
+# Clean up the temporary directory
+rm -rf $PANDOC_DATA_DIR
