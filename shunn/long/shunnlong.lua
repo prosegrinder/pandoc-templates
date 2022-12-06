71d6e9e47ec142a763fff1398b468ba1a008ced0
Callie Cameron <cjcameron7@gmail.com>
2022-12-06 23:59:53 +0100

    Add a 'verbose' flag. (#24)

--- shunn/long/shunnlong.lua
+++ shunn/long/shunnlong.lua
@@ -6,9 +6,13 @@ require "filters.docx.processheader"
 
 vars = {}
 
-function Pandoc(doc, meta)
+function Pandoc(doc)
   local ossep = package.config:sub(1,1)
   local pandoc_data_dir = os.getenv('PANDOC_DATA_DIR')
+  local verbose = false
+  if doc.meta['shunn_verbose'] == '1' then
+    verbose = true
+  end
 
   pandoc.walk_block(pandoc.Div(doc.blocks), wordcount)
 
@@ -24,19 +28,25 @@ function Pandoc(doc, meta)
   -- https://stackoverflow.com/questions/295052/how-can-i-determine-the-os-of-the-system-from-within-a-lua-script
   if ossep == '/' then
     -- *nix (MacOS, Linux) should have zip available
-    print("Zipping reference.docx using UNIX zip.")
+    if verbose then
+      print("Zipping reference.docx using UNIX zip.")
+    end
     os.execute ("cd "
       .. pandoc_data_dir
       .. "/reference && zip -r ../reference.docx * > /dev/null")
   elseif ossep == '\\' then
     -- Windows should have powershell
-    print("Zipping reference.zip using PowerShell.")
+    if verbose then
+      print("Zipping reference.zip using PowerShell.")
+    end
     os.execute("powershell Compress-Archive -Path "
       .. pandoc_data_dir
       .. "\\reference\\* "
       .. pandoc_data_dir
       .. "\\reference.zip")
-    print("Renaming reference.zip to reference.docx.")
+    if verbose then
+      print("Renaming reference.zip to reference.docx.")
+    end
     os.execute("powershell Rename-Item -Path "
       .. pandoc_data_dir
       .. "\\reference.zip -NewName "
